7821455004b52fb15e2f68c98ae8eef2cee2a700
aluno <eric.borges.br@gmail.com>
2025-10-30 16:14:26 -0300

    A

--- src/main/java/one/piece/demo/services/PirateService.java
+++ src/main/java/one/piece/demo/services/PirateService.java
@@ -1,15 +1,17 @@
 package one.piece.demo.services;
 
+import one.piece.demo.dtos.PirateDtoId;
 import one.piece.demo.models.Pirate;
 import one.piece.demo.models.Race;
 import one.piece.demo.repositories.PirateRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class PirateService {
-    public static void insert pirate
-    private PirateRepository
+    private PirateRepository void insert pirate
+    private PirateRepository= new PirateRepository
     public List<Pirate> findByRace(Race race);
-    public List<Pirate> findByID(Id id);
+    public <Pirate> findByID(PirateDtoId);
 
 }
