450a35529a0c33a57f5c324811398250a2c472e9
aluno <eric.borges.br@gmail.com>
2025-10-30 16:11:15 -0300

    a

--- src/main/java/one/piece/demo/services/PirateService.java
+++ src/main/java/one/piece/demo/services/PirateService.java
@@ -1,4 +1,15 @@
 package one.piece.demo.services;
 
+import one.piece.demo.models.Pirate;
+import one.piece.demo.models.Race;
+import one.piece.demo.repositories.PirateRepository;
+
+import java.util.List;
+
 public class PirateService {
+    public static void insert pirate
+    private PirateRepository
+    public List<Pirate> findByRace(Race race);
+    public List<Pirate> findByID(Id id);
+
 }
